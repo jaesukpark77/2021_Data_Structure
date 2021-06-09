a33c3884440a78139b524f56f706c30899c819c5
jaesukpark77 <jaesukpark77@naver.com>
2021-06-09 15:19:54 +0900

    [15주차] TransitiveClosure

---
+++ [15주차] TransitiveClosure/TransitiveClosure/TransitiveClosure.cpp
@@ -0,0 +1,42 @@
+﻿//전자정보공학부 IT융합과 20170586 박재석
+#include <iostream>
+#include <fstream>
+#include "UAMGraph.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "전자정보공학부 IT융합과 20170586 박재석\n" << endl;
+	const char* inFileName = "graph2.dat";
+	// read graph from a file
+	// first entry is size of graph
+
+	ifstream ifs(inFileName);
+	assert(ifs);
+	int n;
+	ifs >> n;
+	uAMGraph G(n);
+	cout << "Created graph; n = " << G.vertexSize() << endl;
+	int u, v;
+	while (ifs >> u) {
+		ifs >> v;
+		G.addEdge(u-1, v-1);
+	}
+	cout << "Edges in graph: m = " << G.edgeSize() << endl;
+	int step;
+	for (step = 0; step < n; step++)
+		for (u = 0; u < n; u++)
+			for (v = 0; v < n; v++)
+				if (G.edgeMember(u, step) && G.edgeMember(step, v))
+					G.addEdge(u, v);
+	// print results
+	for (u = 0; u < n; u++) {
+		cout << u << "\t: ";
+		for (v = 0; v < n; v++)
+			cout << (G.edgeMember(u, v) ? "T " : "F ");
+		cout << endl;
+	}
+	return 0;
+}
+
---
+++ [15주차] TransitiveClosure/TransitiveClosure/amGraph.h
@@ -0,0 +1,31 @@
+#pragma once
+#include"graph.h"
+#include <assert.h>
+
+class amGraph : public graph {
+public:
+	amGraph(int size);
+	virtual bool edgeMember(int fromV, int toV);
+protected:
+	int** am; // am points to pointers to int
+};
+
+amGraph::amGraph(int size) : graph(size)
+{
+	int i;
+	am = new int* [n]; // make an array of pointers to int
+	assert(am);
+	for (i = 0; i < n; i++) {
+		am[i] = new int[n];  // make an array of ints
+		assert(am[i]);
+		int j;
+		for (j = 0; j < n; j++) // initialize the array to 0
+			am[i][j] = 0;
+	}
+}
+
+bool amGraph::edgeMember(int fromV, int toV)
+{
+	assert(fromV < n&& toV < n&& fromV >= 0 && toV >= 0);
+	return bool(am[fromV][toV] != 0);
+}
